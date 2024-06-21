f15f9bc502f0b37ed82a91b7bc55e8af2c7851ab
Rigelle Cruz <rigellecruz@gmail.com>
2024-06-22 11:14:36 +1200

    create HabitList component

---
+++ src/components/HabitList.tsx
@@ -0,0 +1,14 @@
+const HabitList = () => {
+  return (
+    <div>
+      <h1>Habit List</h1>
+      <ul>
+        <li>Habit 1</li>
+        <li>Habit 2</li>
+        <li>Habit 3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default HabitList
