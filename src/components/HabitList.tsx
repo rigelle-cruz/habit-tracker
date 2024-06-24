77bae407fa3c720244729b9ae06b139477d66cb8
Rigelle Cruz <rigellecruz@gmail.com>
2024-06-24 16:46:08 +1200

    create event to handle inputs made by user

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
 const HabitList = () => {
-  const [list, setList] = useState([])
-  const [input, setInput] = useState('')
+  const [list, setList] = useState<string[]>([])
+  const [input, setInput] = useState<string>('')
 
   const handleAdd = () => {
     if (input.trim()) {
@@ -11,11 +11,17 @@ const HabitList = () => {
     }
   }
 
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+  }
+
   return (
     <div>
       <h1>Habit List</h1>
+      <input type="text" value={input} onChange={handleInput} />
+      <button onClick={handleAdd}>Add!</button>
+
       <ul>
-        <button onClick={handleAdd}>Add!</button>
         <li>Habit 1</li>
         <li>Habit 2</li>
         <li>Habit 3</li>
