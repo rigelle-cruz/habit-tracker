ca8dbc6e024e4907c4a05c650a624b7e5b4da5cc
Rigelle Cruz <rigellecruz@gmail.com>
2024-06-24 16:38:04 +1200

    create handleAdd event

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react'
+
 const HabitList = () => {
+  const [list, setList] = useState([])
+  const [input, setInput] = useState('')
+
+  const handleAdd = () => {
+    if (input.trim()) {
+      setList([...list, input])
+      setInput('')
+    }
+  }
+
   return (
     <div>
       <h1>Habit List</h1>
       <ul>
+        <button onClick={handleAdd}>Add!</button>
         <li>Habit 1</li>
         <li>Habit 2</li>
         <li>Habit 3</li>
