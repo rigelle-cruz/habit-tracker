10eba6f688793e1878d289630773872a996c0807
Rigelle Cruz <rigellecruz@gmail.com>
2024-07-08 16:18:37 +1200

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import './App.css';
-import HabitList from './components/HabitList';
+import './App.css'
+import HabitList from './components/HabitList'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Habit Tracker</h1>
         <HabitList />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
