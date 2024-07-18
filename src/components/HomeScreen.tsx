5269b62a1a7998f55fd4f529fa1992cd17530b41
Rigelle Cruz <rigellecruz@gmail.com>
2024-07-18 15:08:49 +1200

    update HomeScreen.tsx

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -2,8 +2,13 @@ type HomeScreenProps = {
 	onItemClick: () => void;
 };
 
-const HomeScreen = () => {
-	return <div>HomeScreen</div>;
+const HomeScreen = ({ onItemClick }: HomeScreenProps) => {
+	return (
+		<div className="home-screen">
+			<h1>Welcome!</h1>
+			<img src="" alt="Click to habits" onClick={onItemClick} />
+		</div>
+	);
 };
 
 export default HomeScreen;
