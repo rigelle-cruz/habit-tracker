b7c547394c3cf4069e8b8a24505ca214a5834739
Rigelle Cruz <rigellecruz@gmail.com>
2024-07-31 15:18:42 +1200

    create Plant component

---
+++ src/components/Plant.tsx
@@ -0,0 +1,12 @@
+export const Plant = () => {
+
+  // Users get assigned a random plant seed.
+  // Whenever a habit is completed, it helps the seed grow into a plant ---> to promote productivity
+  // They can view their collection of plants over time.
+
+  return (
+    <div>Plant Here!</div>
+  )
+}
+
+
