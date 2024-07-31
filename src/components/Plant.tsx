d0d4e27f772b90390c617e135da7d9bda30b6217
Rigelle Cruz <rigellecruz@gmail.com>
2024-08-01 11:55:30 +1200

    pseudocode for plant growth feature

--- src/components/Plant.tsx
+++ src/components/Plant.tsx
@@ -1,6 +1,9 @@
 export const Plant = () => {
 
   // Users get assigned a random plant seed.
+  // Every 10 habit they complete, increment the plant's level
+  // Pass the current level and image source according to the level
+  // Whenever the plant reaches level 4, notify the user about the new seed
   // Whenever a habit is completed, it helps the seed grow into a plant ---> to promote productivity
   // They can view their collection of plants over time.
 
