c5a1f80703208894a486492dcc361ddd90903483
Rigelle Cruz <rigellecruz@gmail.com>
2024-08-06 12:11:05 +1200

    returns the correct plant asset according to level

--- src/components/Plant.tsx
+++ src/components/Plant.tsx
@@ -1,26 +1,21 @@
 type PlantProps = {
-  level:number;
-}
+	level: number;
+};
 
 const plantAssets = [
-  'public/images/plant/temp-plant-1.jpg',
-  'public/images/plant/temp-plant-2.jpg',
-  'public/images/plant/temp-plant-3.jpg',
-  'public/images/plant/temp-plant-4.jpg',
-]
+	'public/images/plant/temp-plant-1.jpg',
+	'public/images/plant/temp-plant-2.jpg',
+	'public/images/plant/temp-plant-3.jpg',
+	'public/images/plant/temp-plant-4.jpg',
+];
 
-export const Plant = () => {
+export const Plant = ({level}: PlantProps) => {
+	// Users get assigned a random plant seed.
+	// Every 10 habit they complete, increment the plant's level
+	// Pass the current level and image source according to the level
+	// Whenever the plant reaches level 4, notify the user about the new seed
+	// Whenever a habit is completed, it helps the seed grow into a plant ---> to promote productivity
+	// They can view their collection of plants over time.
 
-  // Users get assigned a random plant seed.
-  // Every 10 habit they complete, increment the plant's level
-  // Pass the current level and image source according to the level
-  // Whenever the plant reaches level 4, notify the user about the new seed
-  // Whenever a habit is completed, it helps the seed grow into a plant ---> to promote productivity
-  // They can view their collection of plants over time.
-
-  return (
-    <div>Plant Here!</div>
-  )
-}
-
-
+	return <div><img src={plantAssets[level + 1]} alt={`Plant level ${level}`} /></div>;
+};
