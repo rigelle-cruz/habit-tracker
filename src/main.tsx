9f966ceddf250f8dc603861a43440e74eafa4520
Rigelle Cruz <rigellecruz@gmail.com>
2024-08-16 12:45:51 +1200

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
