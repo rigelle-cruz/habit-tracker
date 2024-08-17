2bc8d1a945979be2325127db0a03c2e08b4e6b67
Rigelle Cruz <rigellecruz@gmail.com>
2024-08-17 13:52:09 +1200

    create link to habit list

--- src/components/PlantCollection.tsx
+++ src/components/PlantCollection.tsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const PlantCollection = () => {
 	// displays all the fully grown plants
 
-	return <div>PlantCollection</div>;
+	return (
+		<div>
+			<h2>VIEW ALL PLANT COLLECTION</h2>
+			<Link to="/habits">
+				<button className="btn btn-secondary pixel-corners-no-border">
+					View List!
+				</button>
+			</Link>
+		</div>
+	);
 };
 
 export default PlantCollection;
