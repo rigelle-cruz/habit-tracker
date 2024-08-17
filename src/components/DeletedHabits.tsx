3acf1c756e75679639bcafbed66063cbf2dae484
Rigelle Cruz <rigellecruz@gmail.com>
2024-08-17 13:50:53 +1200

    create link to habit list

--- src/components/DeletedHabits.tsx
+++ src/components/DeletedHabits.tsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const DeletedHabits = () => {
-	return <div>DeletedHabits</div>;
+	return (
+		<div>
+			<h2>LIST OF DELETED HABITS HERE!</h2>
+			<Link to="/habits">
+				<button className="btn btn-secondary pixel-corners-no-border">
+					View List!
+				</button>
+			</Link>
+		</div>
+	);
 };
 
 export default DeletedHabits;
