637036782859f194d256c5968ac18c62633ab725
Rigelle Cruz <rigellecruz@gmail.com>
2024-08-28 10:39:43 +1200

    remove comments

--- src/components/Plant.tsx
+++ src/components/Plant.tsx
@@ -38,17 +38,6 @@ const plantAssets = [
 ];
 
 const Plant = ({ level }: PlantProps) => {
-	// Users get assigned a random plant seed.
-	// Every 10 habit they complete, increment the plant's level
-	// Pass the current level and image source according to the level
-	// Whenever the plant reaches level 4, notify the user about the new seed
-	// Whenever a habit is completed, it helps the seed grow into a plant ---> to promote productivity
-	// They can view their collection of plants over time.
-
-	// There are 5 groups of plant assets. Therefore, users get assigned a random plant to grow. So they get a 'new seed'.
-
-	// To assign a user a new plant I randomly assign them a random number which signifies a particular group of assets. Each group are represent a particular plant type
-
 	const [selectedPlantType, setSelectedPlantType] = useState<number>(0);
 
 	useEffect(() => {
