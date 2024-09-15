85cc126420d79e2e7ada326b8739e4caa6fa1740
Rell  Cruz <rigellecruz@gmail.com>
2024-09-15 12:41:22 +1200

    make function to recover deleted habits

--- src/models/deletedHabitsUtils.ts
+++ src/models/deletedHabitsUtils.ts
@@ -1,25 +1,49 @@
-export type DeletedHabit = {
-	id: string;
-	text: string;
-	completed: boolean;
-};
+import {
+  getHabitsFromLocalStorage,
+  setHabitsInLocalStorage,
+} from './localStorageUtils'
 
-const DELETED_HABITS_KEY = 'deletedHabits';
+export type DeletedHabit = {
+  id: string
+  text: string
+  completed: boolean
+}
+
+const DELETED_HABITS_KEY = 'deletedHabits'
 
 // To retrieve and parse deleted habits from local storage
 export function getDeletedHabitsFromLocalStorage(): DeletedHabit[] {
-	const habits = localStorage.getItem(DELETED_HABITS_KEY);
-	return habits ? JSON.parse(habits) : [];
+  const habits = localStorage.getItem(DELETED_HABITS_KEY)
+  return habits ? JSON.parse(habits) : []
 }
 
 // To set deleted habits in local storage
 export function setDeletedHabitsInLocalStorage(habits: DeletedHabit[]) {
-	localStorage.setItem(DELETED_HABITS_KEY, JSON.stringify(habits));
+  localStorage.setItem(DELETED_HABITS_KEY, JSON.stringify(habits))
 }
 
 // To add a new deleted habit to local storage
 export function addDeletedHabitToLocalStorage(habit: DeletedHabit): void {
-	const habits = getDeletedHabitsFromLocalStorage();
-	habits.push(habit);
-	setDeletedHabitsInLocalStorage(habits);
+  const habits = getDeletedHabitsFromLocalStorage()
+  habits.push(habit)
+  setDeletedHabitsInLocalStorage(habits)
+}
+
+// Recovers deleted habit to active habit list
+export function recoverDeletedHabitToActiveList(habit: DeletedHabit): void {
+  const deletedHabits = getDeletedHabitsFromLocalStorage()
+
+  const updatedDeletedHabits = deletedHabits.filter((h) => h.id !== habit.id)
+  setDeletedHabitsInLocalStorage(updatedDeletedHabits)
+
+  const activeHabit = {
+    id: habit.id,
+    text: habit.text,
+    completed: habit.completed,
+  }
+
+  const activeHabits = getHabitsFromLocalStorage()
+
+  activeHabits.push(activeHabit)
+  setHabitsInLocalStorage(activeHabits)
 }
