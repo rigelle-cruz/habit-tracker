09610fb57ea9f7c7a537c33818f64b138dfa2a5a
Rigelle Cruz <rigellecruz@gmail.com>
2024-10-29 10:29:20 +1300

    Used Flexbox to align plant image and habit tracker side-by-side on larger screens

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,256 +1,258 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import {
-  DragDropContext,
-  Droppable,
-  Draggable,
-  DropResult,
-} from '@hello-pangea/dnd'
+	DragDropContext,
+	Droppable,
+	Draggable,
+	DropResult,
+} from '@hello-pangea/dnd';
 import {
-  addHabitToLocalStorage,
-  addPlantToCollection,
-  getHabitsFromLocalStorage,
-  removeHabitFromLocalStorage,
-  setHabitsInLocalStorage,
-  updateHabitInLocalStorage,
-} from '../models/localStorageUtils.ts'
-import { HabitSchema, Habit } from '../models/habitSchema.ts'
+	addHabitToLocalStorage,
+	addPlantToCollection,
+	getHabitsFromLocalStorage,
+	removeHabitFromLocalStorage,
+	setHabitsInLocalStorage,
+	updateHabitInLocalStorage,
+} from '../models/localStorageUtils.ts';
+import { HabitSchema, Habit } from '../models/habitSchema.ts';
 import {
-  addDeletedHabitToLocalStorage,
-  getDeletedHabitsFromLocalStorage,
-} from '../models/deletedHabitsUtils.ts'
-import Plant from './Plant'
+	addDeletedHabitToLocalStorage,
+	getDeletedHabitsFromLocalStorage,
+} from '../models/deletedHabitsUtils.ts';
+import Plant from './Plant';
 
 const HabitList = () => {
-  const [list, setList] = useState<Habit[]>([])
-  const [input, setInput] = useState<string>('')
-  const [editIndex, setEditIndex] = useState<number | null>(null)
-  const [editingValue, setEditingValue] = useState<string>('')
-  const [showCompleted, setShowCompleted] = useState<boolean>(false)
-  const [completedCount, setCompletedCount] = useState<number>(0)
-  const [plantLevel, setPlantLevel] = useState<number>(1)
+	const [list, setList] = useState<Habit[]>([]);
+	const [input, setInput] = useState<string>('');
+	const [editIndex, setEditIndex] = useState<number | null>(null);
+	const [editingValue, setEditingValue] = useState<string>('');
+	const [showCompleted, setShowCompleted] = useState<boolean>(false);
+	const [completedCount, setCompletedCount] = useState<number>(0);
+	const [plantLevel, setPlantLevel] = useState<number>(1);
 
-  useEffect(() => {
-    const savedHabits = getHabitsFromLocalStorage()
-    const parsedHabits = HabitSchema.array().safeParse(savedHabits)
+	useEffect(() => {
+		const savedHabits = getHabitsFromLocalStorage();
+		const parsedHabits = HabitSchema.array().safeParse(savedHabits);
 
-    if (parsedHabits.success) {
-      setList(parsedHabits.data)
-    } else {
-      console.error(
-        'Error loading habits from localStorage:',
-        parsedHabits.error
-      )
-    }
-  }, [])
+		if (parsedHabits.success) {
+			setList(parsedHabits.data);
+		} else {
+			console.error(
+				'Error loading habits from localStorage:',
+				parsedHabits.error
+			);
+		}
+	}, []);
 
-  const handleAdd = () => {
-    if (input.trim()) {
-      const newItem: Habit = {
-        id: Date.now().toString(),
-        text: input,
-        completed: false,
-      }
-      setList((prevList) => [...prevList, newItem])
-      addHabitToLocalStorage(newItem)
-      setInput('')
-    }
-  }
+	const handleAdd = () => {
+		if (input.trim()) {
+			const newItem: Habit = {
+				id: Date.now().toString(),
+				text: input,
+				completed: false,
+			};
+			setList((prevList) => [...prevList, newItem]);
+			addHabitToLocalStorage(newItem);
+			setInput('');
+		}
+	};
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value)
-  }
+	const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInput(e.target.value);
+	};
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      handleAdd()
-    }
-  }
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			handleAdd();
+		}
+	};
 
-  const handleDelete = (index: number) => {
-    const habitId = list[index].id
-    const habitToDelete = list[index]
-    const newList = list.filter((_, i) => i !== index)
-    setList(newList)
-    addDeletedHabitToLocalStorage(habitToDelete)
-    removeHabitFromLocalStorage(habitId)
-  }
+	const handleDelete = (index: number) => {
+		const habitId = list[index].id;
+		const habitToDelete = list[index];
+		const newList = list.filter((_, i) => i !== index);
+		setList(newList);
+		addDeletedHabitToLocalStorage(habitToDelete);
+		removeHabitFromLocalStorage(habitId);
+	};
 
-  const handleEdit = (index: number) => {
-    setEditIndex(index)
-    setEditingValue(list[index].text)
-  }
+	const handleEdit = (index: number) => {
+		setEditIndex(index);
+		setEditingValue(list[index].text);
+	};
 
-  const handleEditChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEditingValue(e.target.value)
-  }
+	const handleEditChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setEditingValue(e.target.value);
+	};
 
-  const handleEditSubmit = (index: number) => {
-    const updatedHabit = { ...list[index], text: editingValue }
-    const newList = list.map((item, i) => (i === index ? updatedHabit : item))
-    setList(newList)
-    updateHabitInLocalStorage(updatedHabit)
-    setEditIndex(null)
-  }
+	const handleEditSubmit = (index: number) => {
+		const updatedHabit = { ...list[index], text: editingValue };
+		const newList = list.map((item, i) => (i === index ? updatedHabit : item));
+		setList(newList);
+		updateHabitInLocalStorage(updatedHabit);
+		setEditIndex(null);
+	};
 
-  const handleComplete = (index: number) => {
-    const updatedHabit = { ...list[index], completed: !list[index].completed }
-    const newList = list.map((item, i) => (i === index ? updatedHabit : item))
-    setList(newList)
-    updateHabitInLocalStorage(updatedHabit)
+	const handleComplete = (index: number) => {
+		const updatedHabit = { ...list[index], completed: !list[index].completed };
+		const newList = list.map((item, i) => (i === index ? updatedHabit : item));
+		setList(newList);
+		updateHabitInLocalStorage(updatedHabit);
 
-    // Retrieve active and deleted habits
-    const activeHabits = getHabitsFromLocalStorage()
-    const deletedHabits = getDeletedHabitsFromLocalStorage()
-    const allHabits = [...activeHabits, ...deletedHabits]
+		// Retrieve active and deleted habits
+		const activeHabits = getHabitsFromLocalStorage();
+		const deletedHabits = getDeletedHabitsFromLocalStorage();
+		const allHabits = [...activeHabits, ...deletedHabits];
 
-    const newCompletedCount = allHabits.filter(
-      (habit) => habit.completed
-    ).length
+		const newCompletedCount = allHabits.filter(
+			(habit) => habit.completed
+		).length;
 
-    // Calculate the plant type and level
-    const milestone = 40
-    const remainder = newCompletedCount % milestone
-    const plantTypeIndex = Math.floor(newCompletedCount / milestone) % 5
+		// Calculate the plant type and level
+		const milestone = 40;
+		const remainder = newCompletedCount % milestone;
+		const plantTypeIndex = Math.floor(newCompletedCount / milestone) % 5;
 
-    let newLevel = Math.floor(remainder / 10) + 1
-    newLevel = Math.min(newLevel, 4)
+		let newLevel = Math.floor(remainder / 10) + 1;
+		newLevel = Math.min(newLevel, 4);
 
-    if (remainder === 0 && newCompletedCount > 0) {
-      alert('NEW SEED AVAILABLE!!')
+		if (remainder === 0 && newCompletedCount > 0) {
+			alert('NEW SEED AVAILABLE!!');
 
-      const imagePath = `/images/plant/plant-type-${plantTypeIndex + 1}-4.png`
-      addPlantToCollection(imagePath)
-    }
+			const imagePath = `/images/plant/plant-type-${plantTypeIndex + 1}-4.png`;
+			addPlantToCollection(imagePath);
+		}
 
-    setCompletedCount(remainder)
-    setPlantLevel(newLevel)
-  }
+		setCompletedCount(remainder);
+		setPlantLevel(newLevel);
+	};
 
-  // Handles Drag and Drop functionality
-  const onDragEnd = (result: DropResult) => {
-    const { destination, source } = result
-    if (!destination) return
+	// Handles Drag and Drop functionality
+	const onDragEnd = (result: DropResult) => {
+		const { destination, source } = result;
+		if (!destination) return;
 
-    const updatedList = Array.from(list)
-    const [removed] = updatedList.splice(source.index, 1)
-    updatedList.splice(destination.index, 0, removed)
+		const updatedList = Array.from(list);
+		const [removed] = updatedList.splice(source.index, 1);
+		updatedList.splice(destination.index, 0, removed);
 
-    setList(updatedList)
+		setList(updatedList);
 
-    setHabitsInLocalStorage(updatedList)
-  }
+		setHabitsInLocalStorage(updatedList);
+	};
 
-  const filteredList = showCompleted
-    ? list.filter((habit) => habit.completed)
-    : list
+	const filteredList = showCompleted
+		? list.filter((habit) => habit.completed)
+		: list;
 
-  return (
-    <div>
-      <div className="navigation">
-        <Link to="/deleted-habits">
-          <button className="btn btn-secondary pixel-corners-no-border">
-            View Deleted Habits
-          </button>
-        </Link>
+	return (
+		<div>
+			<div className="navigation">
+				<Link to="/deleted-habits">
+					<button className="btn btn-secondary pixel-corners-no-border">
+						View Deleted Habits
+					</button>
+				</Link>
 
-        <Link to="/plant-collection">
-          <button className="btn btn-secondary pixel-corners-no-border">
-            View Plant Collection
-          </button>
-        </Link>
-      </div>
+				<Link to="/plant-collection">
+					<button className="btn btn-secondary pixel-corners-no-border">
+						View Plant Collection
+					</button>
+				</Link>
+			</div>
+			<div className="habit-tracker-container">
+				<div className="habit-container pixel-corners">
+					<h1>Habit Tracker</h1>
+					<div className="input-group">
+						<input
+							className="pixel-corners-no-border"
+							type="text"
+							value={input}
+							onChange={handleInput}
+							onKeyDown={handleKeyDown}
+							placeholder="Enter a new habit..."
+						/>
+						<button className="pixel-corners-no-border" onClick={handleAdd}>
+							Add
+						</button>
+					</div>
 
-      <div className="habit-container pixel-corners">
-        <h1>Habit Tracker</h1>
-        <div className="input-group">
-          <input
-            className="pixel-corners-no-border"
-            type="text"
-            value={input}
-            onChange={handleInput}
-            onKeyDown={handleKeyDown}
-            placeholder="Enter a new habit..."
-          />
-          <button className="pixel-corners-no-border" onClick={handleAdd}>
-            Add
-          </button>
-        </div>
+					<DragDropContext onDragEnd={onDragEnd}>
+						<Droppable droppableId="habits">
+							{(provided) => (
+								<ul
+									className="habit-list"
+									{...provided.droppableProps}
+									ref={provided.innerRef}>
+									{filteredList.map((item, index) => (
+										<Draggable
+											key={item.id}
+											draggableId={item.id}
+											index={index}>
+											{(provided) => (
+												<li
+													ref={provided.innerRef}
+													{...provided.draggableProps}
+													{...provided.dragHandleProps}
+													className={`habit-item pixel-corners-no-border ${
+														item.completed ? 'completed' : ''
+													}`}>
+													{editIndex === index ? (
+														<>
+															<input
+																type="text"
+																value={editingValue}
+																onChange={handleEditChange}
+																onKeyDown={(e) => {
+																	if (e.key === 'Enter')
+																		handleEditSubmit(index);
+																}}
+															/>
+															<button onClick={() => handleEditSubmit(index)}>
+																Save
+															</button>
+														</>
+													) : (
+														<>
+															<input
+																type="checkbox"
+																checked={item.completed}
+																onChange={() => handleComplete(index)}
+															/>
+															<span>{item.text}</span>
+															<div>
+																<button onClick={() => handleEdit(index)}>
+																	<i className="fas fa-edit"></i>
+																</button>
+																<button onClick={() => handleDelete(index)}>
+																	<i className="fas fa-trash"></i>
+																</button>
+															</div>
+														</>
+													)}
+												</li>
+											)}
+										</Draggable>
+									))}
+									{provided.placeholder}
+								</ul>
+							)}
+						</Droppable>
+					</DragDropContext>
 
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="habits">
-            {(provided) => (
-              <ul
-                className="habit-list"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {filteredList.map((item, index) => (
-                  <Draggable key={item.id} draggableId={item.id} index={index}>
-                    {(provided) => (
-                      <li
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        className={`habit-item pixel-corners-no-border ${
-                          item.completed ? 'completed' : ''
-                        }`}
-                      >
-                        {editIndex === index ? (
-                          <>
-                            <input
-                              type="text"
-                              value={editingValue}
-                              onChange={handleEditChange}
-                              onKeyDown={(e) => {
-                                if (e.key === 'Enter') handleEditSubmit(index)
-                              }}
-                            />
-                            <button onClick={() => handleEditSubmit(index)}>
-                              Save
-                            </button>
-                          </>
-                        ) : (
-                          <>
-                            <input
-                              type="checkbox"
-                              checked={item.completed}
-                              onChange={() => handleComplete(index)}
-                            />
-                            <span>{item.text}</span>
-                            <div>
-                              <button onClick={() => handleEdit(index)}>
-                                <i className="fas fa-edit"></i>
-                              </button>
-                              <button onClick={() => handleDelete(index)}>
-                                <i className="fas fa-trash"></i>
-                              </button>
-                            </div>
-                          </>
-                        )}
-                      </li>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
+					<button
+						className="pixel-corners-no-border completed-button"
+						onClick={() => setShowCompleted(!showCompleted)}>
+						{showCompleted ? 'Show All' : 'Show Completed'}
+					</button>
+				</div>
 
-        <button
-          className="pixel-corners-no-border completed-button"
-          onClick={() => setShowCompleted(!showCompleted)}
-        >
-          {showCompleted ? 'Show All' : 'Show Completed'}
-        </button>
-      </div>
+				<div className="plant-section">
+					<Plant level={plantLevel} key={completedCount} />
+				</div>
+			</div>
+		</div>
+	);
+};
 
-      <div className="plant-section">
-        <Plant level={plantLevel} key={completedCount} />
-      </div>
-    </div>
-  )
-}
-
-export default HabitList
+export default HabitList;
